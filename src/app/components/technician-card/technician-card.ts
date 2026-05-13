5628cb81a5cdf627413a55cb9e61ba42196b3808
Justin <justintywater@gmail.com>
2026-05-13 16:39:44 -0500

    styled dummy tech card

---
+++ src/app/components/technician-card/technician-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technician-card',
+  imports: [],
+  templateUrl: './technician-card.html',
+  styleUrl: './technician-card.css',
+})
+export class TechnicianCard {}
--- src/app/technician-overview/technician-overview.ts
+++ src/app/technician-overview/technician-overview.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TechnicianCard } from '../components/technician-card/technician-card';
 
 @Component({
   selector: 'app-technician-overview',
-  imports: [],
+  imports: [TechnicianCard],
   templateUrl: './technician-overview.html',
   styleUrl: './technician-overview.css',
 })
